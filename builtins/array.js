530ad90a0be796fc63fa9b81fe0567e168edbbdb
Mohit Gawande <mohitgawande65@gmail.com>
2025-11-05 13:11:53 +0530

    added array built-ins folder and gitignore

---
+++ builtins/array.js
@@ -0,0 +1,9 @@
+// Examples and reference of Array built-in methods (ES modules, exports).
+const base = [1, 2, 3, 4, 5];
+
+
+function arrayMethods() {
+    console.log('Array built-ins methods:');
+}
+
+export default arrayMethods;
---
+++ index.js
@@ -0,0 +1,8 @@
+import array from './builtins/array.js';
+
+function runApp() {
+  console.log("Hello, JavaScript!");
+  array(); // This will run the array demo function
+}
+
+runApp();
